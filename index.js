79769866c91feaadcb35cc8ed08a05fc4a38fc8a
Aleksi Tamminen <aleksi@aleksitamminen.fi>
2021-09-22 23:30:59 +0300

    Javaa

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const fahrenheitTuloste = document.querySelector('input')
+const fahrenheitTuloste = document.querySelector('fahrenheit')
+const fahrenheitTuloste = document.querySelector('celsius')
+const button = document.querySelector('button');
+
+function muunna(){
+    alert('Juupa juu!')  
+  
+  }
+
+muunnaButton.addEventListener('click' , muunna);
